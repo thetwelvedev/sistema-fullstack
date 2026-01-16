bc75d206b7540a6a7f30bd659bfd7e020c2ccfae
Guilherme Almeida <guilhermealmeidadaluz5@gmail.com>
2026-01-15 23:48:16 -0400

    feat: alterações no frontend

---
+++ barbershop-frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Barbershop">
+        <div className='display-4'>Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema para barbearia</p>
+    </Main>
